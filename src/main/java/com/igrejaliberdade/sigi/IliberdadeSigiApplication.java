f78420cb1b11ddae586ef11660b3f3138eb4fb48
Bruno Costa <bruno.sc@outlook.com.br>
2018-07-14 02:09:18 -0300

    Initial commit

---
+++ src/main/java/com/igrejaliberdade/sigi/IliberdadeSigiApplication.java
@@ -0,0 +1,12 @@
+package com.igrejaliberdade.sigi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IliberdadeSigiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IliberdadeSigiApplication.class, args);
+	}
+}
